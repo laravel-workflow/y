743ede0bc131030143d742dc7675ca656beb5393
Richard McDaniel <richard.lee.mcdaniel@gmail.com>
2022-12-15 14:57:10 -0600

    First commit

---
+++ tests/Unit/SerializeTest.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use Y\Y;
+
+final class TestSerialize extends TestCase
+{
+    /**
+     * @dataProvider dataProvider
+     */
+    public function testSerialize($data): void
+    {
+        $unserialized = Y::unserialize(Y::serialize($data));
+        $this->assertSame($data, $unserialized);
+    }
+
+    public function dataProvider(): array
+    {
+        return [
+            'array []' => [[]],
+            'array [[]]' => [[[]]],
+            'array assoc' => [
+                'key' => 'value',
+            ],
+            'int(PHP_INT_MIN)' => [PHP_INT_MIN],
+            'int(-1)' => [-1],
+            'int(0)' => [0],
+            'int(1)' => [1],
+            'int(PHP_INT_MAX)' => [PHP_INT_MAX],
+            'float' => [1.123456789],
+            'null' => [null],
+            'string empty' => [''],
+            'string foo' => ['foo'],
+            'string bytes' => [random_bytes(4096)],
+            'bool' => [true, false],
+        ];
+    }
+}
